2f656070769b3ee831b62454766be80c66bc10cc
lucasauguto <lucasaugustonobre@gmail.com>
2025-06-04 13:25:04 -0300

    Commit inicial

---
+++ database/cliente.py
@@ -0,0 +1,9 @@
+# Este é um banco de dados fake (para testes)
+CLIENTES = [
+    {"id": 1, "nome":"Gulherme", "email":"teste@teste.com"},
+    {"id": 2, "nome":"Jorge", "email":"asd@teste.com"},
+    {"id": 3, "nome":"Maria", "email":"tasdasd@teste.com"},
+    {"id": 4, "nome":"Felipe", "email":"sdagfae@teste.com"},
+    {"id": 5, "nome":"João", "email":"asfasf@teste.com"},
+    {"id": 6, "nome":"Julia", "email":"qweqrtq@teste.com"},
+]
---
+++ routes/cliente.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, render_template, request
+from database.cliente import CLIENTES
+cliente_route = Blueprint('cliente', __name__)
+"""
+    Rota de Clientes
+    
+   - /clientes/ (GET)- Listar os clientes
+   - /clientes/ (POST) - inserir o cliente no servidor
+   - /clientes/new (GET) - renderizar o formulário para criar um cliente
+   - /clientes/<id> (GET) - Obter os dados de um cliente
+   - /clientes/<id>/edit (GET)- renderizar um formulario para editar um cliente
+   - /clientes/<id>/update (PUT) - atualizar os dados do cliente
+   - /clientes/<id>/delete (DELETE) - deleta o registo do usuario
+    
+"""
+@cliente_route.route('/')
+def lista_clientes():
+    """ listas os clientes """
+    return render_template('lista_clientes.html', clientes=CLIENTES)
+
+@cliente_route.route('/', methods=['POST'])
+def inserir_cliente():
+    """ inserir os dados do cliente """
+    data = request.json
+    
+    novo_usuario = {
+        "id" : len(CLIENTES) + 1,
+        "nome": data['nome'],
+        "email" : data['email']
+    }
+    
+    CLIENTES.append(novo_usuario)
+    
+    return render_template('item_cliente.html', cliente=novo_usuario)
+
+@cliente_route.route('/new')
+def form_cliente():
+    """ formulario para cadastrar um cliente """
+    return render_template('form_cliente.html')
+
+@cliente_route.route('/<int:cliente_id>')
+def detalhe_cliente(cliente_id):
+    """ exibir detalhes de um cliente """
+    return render_template('detalhe_cliente.html')
+
+@cliente_route.route('/<int:cliente_id>/edit')
+def form_edit_cliente(cliente_id):
+    """ exibir detalhes de um cliente """
+    return render_template('form_edit_cliente.html')
+
+
+@cliente_route.route('/<int:cliente_id>/update', methods = ['PUT'])
+def atualizar_cliente(cliente_id):
+    """ atualizar infomacoes do cliente """
+    pass
+
+@cliente_route.route('/<int:cliente_id>/deletar', methods=(['DELETE']))
+def deletar_cliente(cliente_id):
+    """ deletar informacoes do cliente """
+    pass
